78d3e7ff8a92af252976f987fb53d03ad3609f4f
Michel-André Chirita <michel.chirita@gmail.com>
2019-02-02 19:50:57 +0100

    First commit

---
+++ TouchingTale/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouchingTale
+//
+//  Created by Michel-Andre Chirita on 25/01/2019.
+//  Copyright © 2019 croamac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
